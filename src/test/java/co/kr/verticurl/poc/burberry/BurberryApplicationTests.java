206726f2459da1ecd485a6f1ffce96348204e1dd
rudanata1 <rudanata1@gmail.com>
2023-05-15 17:25:19 +0900

    Initial commit

---
+++ src/test/java/co/kr/verticurl/poc/burberry/BurberryApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.verticurl.poc.burberry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BurberryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
